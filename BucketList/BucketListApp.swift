10557a27e36409b406aabe4067ee1d02c648fb00
Santhosh2141 <snthshsrinivas@gmail.com>
2025-09-20 20:07:21 +0530

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Santhosh Srinivas on 20/09/25.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
